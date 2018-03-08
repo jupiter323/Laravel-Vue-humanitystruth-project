dea0b9981837af1e7014e0c1327b74526b883b18
MohdUmairRizvi <rizviumair90@gmail.com>
2018-03-08 15:39:48 +0800

    align:fixed image:150*150

--- resources/views/affiliates/index.blade.php
+++ resources/views/affiliates/index.blade.php
@@ -6,13 +6,15 @@
 
 <div class="w3-container w3-padding w3-card-4 w3-round-large w3-margin w3-white">
     <h1>Affilates</h1>
-    <div class="w3-container w3-padding w3-row">
-        <div class="w3-col s12 m10 l7 ">
+    <div class="w3-container w3-padding w3-row align-center">
             @foreach($affiliates as $affiliate)
-                <a href="{{$affiliate->website}}" target="_blank" ><img class="w3-margin-bottom w3-padding" src="{{asset('uploads/'.$affiliate->logo)}}" width="150px" height="60px;"></a>
+            <div class="w3-left w3-margin" style="width:150px;">
+                <a href="{{$affiliate->website}}" target="_blank" >
+                    <img class="" src="{{asset('uploads/'.$affiliate->logo)}}" width="150px" height="150px;">
+                    <p class="w3-margin-bottom w3-center" >{{$affiliate->name}}</p>
+                </a>
+            </div>    
             @endforeach
-        </div>
-
     </div>
 </div>    
         
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,6 +2,7 @@
     <link rel="icon" href="/data/imgs/icons/humanitystruth-favicon.png">
     <link type="text/css" rel="stylesheet" href="/css/w3.css">
     <link type="text/css" rel="stylesheet" href="/css/app.css">
+    <link type="text/css" rel="stylesheet" href="/css/style.css">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script src="/js/functions.js" type="text/javascript"></script>
     <script type="application/ld+json">
